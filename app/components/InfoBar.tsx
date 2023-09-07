73737aeeefff284d7248843f70932082d7025222
xvoorvaa <xander@xvproductions.com>
2023-09-07 23:01:26 +0000

    Rounded

--- app/components/InfoBar.tsx
+++ app/components/InfoBar.tsx
@@ -13,7 +13,7 @@ export default function InfoBar({ objectName, object }: InforCardProps) {
   return (
     <>
       <div className="ml-4 font-bold">{objectName}</div>
-      <div className="bg-gray-300 rounded mt-1 m-4 hover:bg-gray-400">
+      <div className="bg-gray-300 rounded mt-1 m-4 hover:bg-gray-400 hover:rounded">
         <a href={`https://etherscan.io/ensName/${object}`}>
           <p className="truncate text-mg p-4">{object}</p>
         </a>
