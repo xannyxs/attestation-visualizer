af979d7aeefa0f660d9512e674419d26998c13f0
xvoorvaa <xander@xvproductions.com>
2023-10-07 20:44:48 +0200

    Fixed build

--- app/components/ThreeGraph.tsx
+++ app/components/ThreeGraph.tsx
@@ -41,8 +41,8 @@ function buildGraphData(attestations: Attestation[]) {
     if (!addressToLinks.has(source)) addressToLinks.set(source, []);
     if (!addressToLinks.has(target)) addressToLinks.set(target, []);
 
-    addressToLinks.get(source).push(link);
-    addressToLinks.get(target).push(link);
+    addressToLinks.get(source)?.push(link);
+    addressToLinks.get(target)?.push(link);
 
     return link;
   });
