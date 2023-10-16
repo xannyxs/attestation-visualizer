8f1cda6f1934e31f2ff3fafeb29926749d7665de
xvoorvaa <xander@xvproductions.com>
2023-10-16 09:45:03 -0600

    Fixed build

--- app/components/getEnsName.tsx
+++ app/components/getEnsName.tsx
@@ -1,6 +1,6 @@
 import { useEnsName } from "wagmi";
 import { EthereumAddress } from "../types";
 
-export default function (address: EthereumAddress) {
-  const ensName = useEnsName(address as EthereumAddress);
-}
+// export default function (address: EthereumAddress) {
+//   const ensName = useEnsName(address as EthereumAddress);
+// }
