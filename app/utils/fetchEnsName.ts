baf0b3b08f809540f3dcf60d1d5f89765ac96904
xvoorvaa <xander@xvproductions.com>
2023-10-16 12:26:53 -0600

    Returning the correct data

--- app/utils/fetchEnsName.ts
+++ app/utils/fetchEnsName.ts
@@ -8,11 +8,10 @@ export default async function fetchEnsName(
     if (res.ok) {
       const data = await res.json();
 
-      return data && data.ensName ? data.ensName : null;
-    } else {
-      console.log(`Failed to fetch ENS name. Status: ${res.status}`);
-      return null;
+      return data;
     }
+    console.log(`Failed to fetch ENS name. Status: ${res.status}`);
+    return null;
   } catch (error) {
     console.error("An error occurred while fetching the ENS name:", error);
     return null;
