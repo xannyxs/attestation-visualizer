02e2612ccf619d7a2d27dc3530ae3ed75e608d4c
xvoorvaa <xander@xvproductions.com>
2023-11-03 12:39:05 +0300

    Made ListCard more readable

--- app/components/cards/ListCard.tsx
+++ app/components/cards/ListCard.tsx
@@ -14,25 +14,31 @@ export default function ListCard({
   card: CardInfo;
   onIconClick: () => void;
 }) {
-  const dimensions = 55;
   const { openModal } = useContext(ModalContext);
 
+  const handleCardClick = () => {
+    openModal(<ShowNodeCard cardInfo={card} />);
+  };
+
+  const dimensions = 55; 
+
   return (
-    <div className="flex m-2 transition-all cursor-pointer">
-      <div className="shadow-black bg-gray-200 rounded-md flex items-center hover:bg-red-100 hover:shadow-md transition-all">
+    <div className="flex m-2 transition-all cursor-pointer items-center">
+      <div
+        className="shadow-black bg-gray-200 rounded-l-md flex items-center hover:bg-red-100 hover:shadow-md transition-all w-full"
+        onClick={handleCardClick}
+        role="button"
+        aria-label={`Details for ${card.currentAddress}`}
+        tabIndex={0}
+      >
         <Image
           src={image}
-          alt="Avatar"
-          className="rounded-md"
+          alt={`${card.currentAddress} Avatar`}
+          className="rounded-l-md"
           width={dimensions}
           height={dimensions}
         />
-        <div
-          className="flex-grow border-l-2 pl-3 pr-3 flex flex-col justify-center truncate"
-          onClick={() => {
-            openModal(<ShowNodeCard cardInfo={card} />);
-          }}
-        >
+        <div className="flex-grow border-l-2 pl-3 pr-3 flex flex-col justify-center truncate">
           <h2 className="text-md font-semibold truncate">
             Address: {card.currentAddress}
           </h2>
@@ -41,12 +47,13 @@ export default function ListCard({
           </p>
         </div>
       </div>
-      <div
-        className="flex justify-center items-center ml-2 p-2 bg-gray-200 rounded-md hover:bg-red-100 transition-all"
+      <button
+        className="flex justify-center items-center ml-2 p-2 pt-4 pb-4 bg-gray-200 rounded-r-md hover:bg-red-100 transition-all"
         onClick={onIconClick}
+        aria-label="Locate address on map"
       >
         <LocateFixed size={25} />
-      </div>
+      </button>
     </div>
   );
 }
