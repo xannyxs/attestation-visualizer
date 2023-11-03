16cc85709a43c7914bcb3b1bc032e20396708b8b
xvoorvaa <xander@xvproductions.com>
2023-11-03 05:17:26 +0300

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -13,7 +13,9 @@ export default function Home() {
   const [activeView, setActiveView] = useState<
     "grid" | "list" | "credits" | "bug" | "feature" | "none"
   >("none");
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
 
+  const toggleDropdown = () => setDropdownOpen(!isDropdownOpen);
   const handleItemClick = (view: typeof activeView) => {
     if (activeView === view) {
       setActiveView("none");
@@ -50,6 +52,23 @@ export default function Home() {
           {activeView === "grid" && <GridView />}
           {activeView === "list" && <ListView />}
           {activeView === "credits" && <CreditsView />}
+        </div>
+        <div className="text-white absolute top-0 right-0 z-10">
+          <button onClick={toggleDropdown} className="p-2">
+            Test Dropdown
+          </button>
+          {isDropdownOpen && (
+            <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-10">
+              <div className="py-1">
+                <a href="#" className="text-gray-700 block px-4 py-2 text-sm">
+                  Round 2
+                </a>
+                <a href="#" className="text-gray-700 block px-4 py-2 text-sm">
+                  Round 3 (Most recent)
+                </a>
+              </div>
+            </div>
+          )}
         </div>
         <div className="absolute">
           <ThreeGraph />
