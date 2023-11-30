0c3f3809f07afbf6dc4f52c566f8363fbd7beb16
xvoorvaa <xander@xvproductions.com>
2023-11-30 19:45:11 +0100

    Update GridCard.tsx

--- app/components/cards/GridCard.tsx
+++ app/components/cards/GridCard.tsx
@@ -23,7 +23,10 @@ export default function GridCard({
   const dimensions = 75;
 
   return (
-    <div className="shadow-black bg-gray-100 hover:bg-red-100 rounded-md py-1 transition-all cursor-pointer">
+    <div
+      onClick={() => handleCardClick()}
+      className="shadow-black bg-gray-100 hover:bg-red-100 rounded-md py-1 transition-all cursor-pointer"
+    >
       <button
         className="ml-2 mt-1 p-2 rounded-md self-start bg-gray-200 hover:bg-red-300 transition-all"
         onClick={onIconClick}
@@ -31,45 +34,43 @@ export default function GridCard({
       >
         <LocateFixed size={25} />
       </button>
-      <div onClick={() => handleCardClick()}>
-        <div className="photo-wrapper p-2">
-          <Image
-            src={image}
-            alt={`${card.currentAddress} Avatar`}
-            className="rounded-full mx-auto"
-            width={dimensions}
-            height={dimensions}
-          />
-        </div>
-        <div className="px-2">
-          {card.ens ? (
-            <>
-              <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
-                ENS Address
-              </h3>
-              <div className="text-center text-gray-600 text-xs font-semibold">
-                <p className="truncate">{card.ens}</p>
-              </div>
-            </>
-          ) : (
-            <>
-              <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
-                Address
-              </h3>
-              <div className="text-center text-gray-600 text-xs font-semibold">
-                <p className="truncate">{card.currentAddress}</p>
-              </div>
-            </>
-          )}
-          <div className="flex flex-col text-sm my-3">
-            <div className="truncate">
-              <span className="font-bold">Referred:&nbsp;</span>
-              <span className="truncate">{card.referredBy}</span>
+      <div className="photo-wrapper p-2">
+        <Image
+          src={image}
+          alt={`${card.currentAddress} Avatar`}
+          className="rounded-full mx-auto"
+          width={dimensions}
+          height={dimensions}
+        />
+      </div>
+      <div className="px-2">
+        {card.ens ? (
+          <>
+            <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
+              ENS Address
+            </h3>
+            <div className="text-center text-gray-600 text-xs font-semibold">
+              <p className="truncate">{card.ens}</p>
             </div>
-            <div>
-              <span className="font-bold">Referred Method:&nbsp;</span>
-              <span>{card.referredMethod}</span>
+          </>
+        ) : (
+          <>
+            <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
+              Address
+            </h3>
+            <div className="text-center text-gray-600 text-xs font-semibold">
+              <p className="truncate">{card.currentAddress}</p>
             </div>
+          </>
+        )}
+        <div className="flex flex-col text-sm my-3">
+          <div className="truncate">
+            <span className="font-bold">Referred:&nbsp;</span>
+            <span className="truncate">{card.referredBy}</span>
+          </div>
+          <div>
+            <span className="font-bold">Referred Method:&nbsp;</span>
+            <span>{card.referredMethod}</span>
           </div>
         </div>
       </div>
