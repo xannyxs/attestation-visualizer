c650328784247c33e884a0e76162689b975dc206
xvoorvaa <xander@xvproductions.com>
2023-11-30 20:03:42 +0100

    Changed GridCard layout

--- app/components/cards/GridCard.tsx
+++ app/components/cards/GridCard.tsx
@@ -46,21 +46,15 @@ export default function GridCard({
       <div className="px-2">
         {card.ens ? (
           <>
-            <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
-              ENS Address
+            <h3 className="text-center text-md text-gray-900 font-medium leading-8">
+              {card.ens}
             </h3>
-            <div className="text-center text-gray-600 text-xs font-semibold">
-              <p className="truncate">{card.ens}</p>
-            </div>
           </>
         ) : (
           <>
             <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
-              Address
-            </h3>
-            <div className="text-center text-gray-600 text-xs font-semibold">
               <p className="truncate">{card.currentAddress}</p>
-            </div>
+            </h3>
           </>
         )}
         <div className="flex flex-col text-sm my-3">
