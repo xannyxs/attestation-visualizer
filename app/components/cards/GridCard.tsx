68d19b69e5c273fc35ae9d6e7aaea0773896e7e6
xvoorvaa <xander@xvproductions.com>
2023-11-30 20:08:50 +0100

    Update GridCard.tsx

--- app/components/cards/GridCard.tsx
+++ app/components/cards/GridCard.tsx
@@ -46,7 +46,7 @@ export default function GridCard({
       <div className="px-2">
         {card.ens ? (
           <>
-            <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
+            <h3 className="text-center text-md text-gray-900 font-medium leading-8">
               {card.ens}
             </h3>
           </>
