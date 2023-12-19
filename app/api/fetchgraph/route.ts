87cedfc58335fd6fda11f17a2189f2fcba06fd79
xvoorvaa <xander@xvproductions.com>
2023-12-19 17:12:45 +0100

    Added a CONFIG for more straighforward view

--- app/api/fetchgraph/route.ts
+++ app/api/fetchgraph/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse, NextRequest } from "next/server";
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 import { Attestation } from "@/app/types";
+import { c_CLIENT_URI, c_QUERY, c_SCHEMAID, c_ATTESTER } from "@/CONFIG";
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
   const round = request.nextUrl.searchParams.get("round");
@@ -11,49 +12,26 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
   }
 
   const client = new ApolloClient({
-    uri: "https://optimism.easscan.org/graphql",
+    uri: c_CLIENT_URI,
     cache: new InMemoryCache(),
   });
-
-  // Default
-  let fromDateUnixTimestamp = 1697000000;
-  let untilDateUnixTimestamp = Math.floor(Date.now() / 1000);
-  if (round === "2") {
-    console.log("round 2");
-    fromDateUnixTimestamp = 1694304000;
-    untilDateUnixTimestamp = 1694736000;
-  }
 
   try {
     const { data } = await client.query({
       query: gql`
-        query Query($where: AttestationWhereInput) {
-          attestations(where: $where) {
-            attester
-            recipient
-            decodedDataJson
-            timeCreated
-          }
-        }
+        ${c_QUERY}
       `,
       variables: {
         where: {
           AND: [
             {
               schemaId: {
-                equals:
-                  "0xfdcfdad2dbe7489e0ce56b260348b7f14e8365a8a325aef9834818c00d46b31b",
+                equals: c_SCHEMAID,
               },
             },
             {
               revoked: {
                 equals: false,
-              },
-            },
-            {
-              timeCreated: {
-                gte: fromDateUnixTimestamp,
-                lte: untilDateUnixTimestamp,
               },
             },
           ],
@@ -62,8 +40,7 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
     });
 
     let attestationsByAttester = data.attestations.filter(
-      (attestation: any) =>
-        attestation.attester === "0x621477dBA416E12df7FF0d48E14c4D20DC85D7D9",
+      (attestation: any) => attestation.attester === c_ATTESTER,
     );
 
     const recentAttestations = Object.values(attestationsByAttester).map(
