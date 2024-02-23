92206a21bb753088d6f29e63877a203d8bf1b872
xvoorvaa <xander@xvproductions.com>
2024-02-23 16:07:50 +0100

    Fixed build for now

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,13 @@
-const Home = () => { };
+import {
+  Card,
+  CardTitle,
+  CardFooter,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card";
+
+const Home = () => {
+  return <h1>Hello world</h1>;
+};
 
 export default Home;
