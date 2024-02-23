aeea05f8d07babbe05c0e26790d106ea1076a846
xvoorvaa <xander@xvproductions.com>
2024-02-23 16:02:32 +0100

    Added anitalias

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html className="antialiased" lang="en">
       <body className={font.className}>
         <ModalContextProvider>
           <SelectedNodeContextProvider>
