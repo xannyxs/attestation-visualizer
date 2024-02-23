28b65f7c3cfed75ab20ef343c2b30b61fa27954e
xvoorvaa <xander@xvproductions.com>
2024-02-23 16:02:26 +0100

    Added a few modules

---
+++ app/(home)/graph/page.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import React, { useState, useCallback } from "react";
+import SidebarComponent from "../components/Layout/SideBarComponent";
+import ThreeGraph from "../components/ThreeGraphWrapper";
+import { mainnet, WagmiConfig, createConfig } from "wagmi";
+import { createPublicClient, http } from "viem";
+import ListView from "../components/views/List/ListView";
+import GridView from "../components/views/Grid/GridView";
+import CreditsView from "../components/views/Credits/CreditsView";
+import GraphDataProvider from "../components/context/GraphDataContext";
+import RoundDropdown from "../components/RoundDropdown";
+import { ActiveView } from "../types";
+import useIsMobile from "../components/Layout/useIsMobile";
+
+export default function Home() {
+  const isMobile = useIsMobile();
+  const [activeView, setActiveView] = useState<ActiveView>(ActiveView.None);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [round, setRound] = useState(3);
+
+  const handleSelectRound = useCallback((selectedRound: number) => {
+    setRound(selectedRound);
+    setDropdownOpen(false);
+  }, []);
+
+  const toggleDropdown = useCallback(() => {
+    setDropdownOpen(!isDropdownOpen);
+  }, [isDropdownOpen]);
+
+  const handleItemClick = (view: ActiveView) => {
+    if (activeView === view) {
+      setActiveView(ActiveView.None);
+    } else {
+      setActiveView(view);
+    }
+  };
+
+  const handleRoute = useCallback((href: string) => {
+    window.open(href, "_blank");
+  }, []);
+
+  const configWagmi = createConfig({
+    autoConnect: true,
+    publicClient: createPublicClient({
+      chain: mainnet,
+      transport: http(),
+    }),
+  });
+
+  if (isMobile) {
+    return (
+      <div className="flex justify-center text-white text-xl">
+        Mobile version not supported.
+      </div>
+    );
+  }
+
+  return (
+    <WagmiConfig config={configWagmi}>
+      <GraphDataProvider round={round}>
+        <main className="flex">
+          <SidebarComponent
+            handleRoute={handleRoute}
+            activeView={activeView}
+            handleItemClick={handleItemClick}
+          />
+          <div
+            className={`w-[35rem] relative ${
+              activeView === ActiveView.None ? "z-0" : "z-10"
+            }`}
+          >
+            {activeView === ActiveView.Grid && <GridView />}
+            {activeView === ActiveView.List && <ListView />}
+            {activeView === ActiveView.Credits && <CreditsView />}
+          </div>
+          <RoundDropdown
+            isDropdownOpen={isDropdownOpen}
+            toggleDropdown={toggleDropdown}
+            round={round}
+            handleSelectRound={handleSelectRound}
+          />
+          <div className="absolute">
+            <ThreeGraph />
+          </div>
+        </main>
+      </GraphDataProvider>
+    </WagmiConfig>
+  );
+}
