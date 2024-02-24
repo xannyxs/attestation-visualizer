5b6812674c23817cd5e156e1497ff0b15dd82909
xvoorvaa <xander@xvproductions.com>
2024-02-24 16:15:53 +0100

    Reverted a few changes

--- components/views/List/cards/ListCard.tsx
+++ components/views/List/cards/ListCard.tsx
@@ -1,27 +1,9 @@
 import Image from "next/image";
 import { LocateFixed } from "lucide-react";
+import { useContext } from "react";
+import { ModalContext } from "@/components/context/modalContext";
 import ShowNodeCard from "@/components/ShowNodeCard";
-import { ICardProps as CardInfo } from "@/lib/types";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+import { ICardProps as CardInfo } from "@/app/types";
 
 const ListCard = ({
   image,
@@ -32,48 +14,52 @@ const ListCard = ({
   card: CardInfo;
   onIconClick: () => void;
 }) => {
+  const { openModal } = useContext(ModalContext);
+
+  const handleCardClick = () => {
+    openModal(<ShowNodeCard cardInfo={card} />);
+  };
+
   const dimensions = 55;
 
   return (
-    <div className="flex items-center w-full">
-      <Card className="flex items-center m-1 bg-gray-200 transition-all w-[85%]">
-        <AlertDialog>
-          <AlertDialogTrigger className="flex items-center w-full transition-all hover:bg-red-100">
-            <CardHeader>
-              <Image
-                src={image}
-                alt={`${card.currentAddress} Avatar`}
-                className="rounded-l-md"
-                width={dimensions}
-                height={dimensions}
-              />
-            </CardHeader>
-            <CardContent className="flex flex-col border-l-2 truncate">
-              {card.ens ? (
-                <h2 className="font-semibold text-md truncate">
-                  ENS Address: {card.ens}
-                </h2>
-              ) : (
-                <h2 className="font-semibold text-md truncate">
-                  Address: {card.currentAddress}
-                </h2>
-              )}
-              <p className="text-base text-gray-500 truncate">
-                Referred By: {card.referredBy}
-              </p>
-            </CardContent>
-          </AlertDialogTrigger>
-          <AlertDialogContent>
-            <ShowNodeCard cardInfo={card} />
-          </AlertDialogContent>
-        </AlertDialog>
-      </Card>
-      <Button
-        className="flex items-center justify-center rounded-r-md transition-all mx-1 bg-gray-200 hover:bg-red-100 w-[10%]"
-        onClick={onIconClick}
+    <div className="flex items-center m-2 transition-all cursor-pointer">
+      <div
+        className="flex items-center w-full bg-gray-200 rounded-l-md transition-all hover:bg-red-100 hover:shadow-md shadow-black"
+        onClick={handleCardClick}
+        role="button"
+        aria-label={`Details for ${card.currentAddress}`}
+        tabIndex={0}
       >
-        <LocateFixed size={25} className="text-black" />
-      </Button>
+        <Image
+          src={image}
+          alt={`${card.currentAddress} Avatar`}
+          className="rounded-l-md"
+          width={dimensions}
+          height={dimensions}
+        />
+        <div className="flex flex-col flex-grow justify-center pr-3 pl-3 border-l-2 truncate">
+          {card.ens ? (
+            <h2 className="font-semibold text-md truncate">
+              ENS Address: {card.ens}
+            </h2>
+          ) : (
+            <h2 className="font-semibold text-md truncate">
+              Address: {card.currentAddress}
+            </h2>
+          )}
+          <p className="text-base text-gray-500 truncate">
+            Referred By: {card.referredBy}
+          </p>
+        </div>
+      </div>
+      <button
+        className="flex justify-center items-center p-2 pt-4 pb-4 ml-2 bg-gray-200 rounded-r-md transition-all hover:bg-red-100"
+        onClick={onIconClick}
+        aria-label="Locate address on map"
+      >
+        <LocateFixed size={25} />
+      </button>
     </div>
   );
 };
